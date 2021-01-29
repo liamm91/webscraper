e1cac41d4d5c5d9044f0148af65357555787c8c3
Liam Major <liam.major91@gmail.com>
2021-01-29 18:13:52 -0500

    added logger module

---
+++ src/logger.py
@@ -0,0 +1,8 @@
+'''
+############################################## ASIDE ##############################################
+This is the module that will contain all the code for the logging process.
+It will be a neat wrapper that can be interfaced with via. multithreading and the webcrawler
+'''
+
+def f2():
+    print("imported from logger.py")
