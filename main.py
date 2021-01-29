17b705c2b12743987a02a8c538627bdc91a310ae
Liam Major <liam.major91@gmail.com>
2021-01-29 18:13:08 -0500

    added to do list and test code

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# this is the main file for the new web crawler
+"""
+To do:
+
+make function to get the html request
+make web crawler class
+make multi threading
+make logging
+"""
+
+__author__ = "Liam Major"
+__version__ = "0.1"
+
+# verbose = True:
+# def pv(text: str): # pv = print verbose
+#     if verbose:
+#         print(text)
+
+from src import db, logger, crawler
+from src.consts import *
+
+print(crawler.scrape_attrs('<a href="https://stackoverflow.blog" class="fr">company blog</a>'))
+print(crawler.scrape_attrs('<a href="https://stackoverflow.com/help" class="js-gps-track" data-gps-track="site_switcher.click({ item_type:14 })">help</a>'))
