e691b627eabcef71b21097a82083c368cb443801
Liam Major <liam.major91@gmail.com>
2021-01-29 18:13:47 -0500

    added db module

---
+++ src/db.py
@@ -0,0 +1,7 @@
+'''
+############################################## ASIDE ##############################################
+This is the module that contains the database for all the websites visited.
+'''
+
+def f1 ():
+    print("imported from db.py")
